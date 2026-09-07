4182f564d3e20ebe4c9328c0e0a5cad41a3bad6c
yezhiyi9670 <yezhiyi0562@foxmail.com>
2026-09-07 20:21:02 +0800

    Sync latest tileset images

---
+++ build/copy_tilesets.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -euo pipefail
+
+source "$(dirname "$0")/include/vars.sh"
+
+for game in ${games[@]}; do
+    dir="_grabber/tiled-atlasses/$game"
+    if [[ ! -d "$dir" ]]; then
+        mkdir "$dir"
+    fi
+    cp -r "$game/dist/assets/tilemaps/tilesets/"/*.png "$dir"
+    cp -r "$game/dist/assets/tilemaps/tilesets/"/default_level.json "$dir"
+    echo "$dir"
+done
---
+++ build/include/vars.sh
@@ -0,0 +1,3 @@
+games=(
+    A B C D E F G
+)
